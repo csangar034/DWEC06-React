db64e6e3f58ebdbb7e0c1da73079e4ac30799e00
Cristina Sanchez García <cristinasanchezgarcia@MacBook-Pro-de-Cristina.local>
2021-12-29 16:55:00 +0100

    hola mundo y jsx

---
+++ 03-jsx/src/index.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import logoAlberti from './logoAlberti.png';
+
+// CONSTANTES Y VARIABLES
+const name    = "Cristina"            // Variable con un texto
+var user = {                        // Objeto que contiene información sobre un usuario
+  name      : "Cristina",
+  lastName  : "Sánchez",
+  logo      : logoAlberti
+}
+var element = <h1>Hola {name}</h1>
+
+// FUNCIONES
+// Función que devuelve el nombre completo de un usuario
+function formatName(user){
+  return user.name + " " + user.lastName;
+}
+
+// Función que devuelve el saludo con el nombre de un usuario si se conoce o un texto genérico si no se conoce
+//user = "";
+function getGreeting(){
+  return (user) ? <span>{formatName(user)}</span> : <span>desconocido</span>;
+}
+
+// Paso 2: Variable con JSX que hace uso de una función
+element = <h1>Hola {formatName(user)}</h1>
+
+// Paso 3: Variable JSX haciendo uso de una función con condicional
+element = <h1>Hola {getGreeting(user)}</h1>
+
+// Paso 4: Si necesitamos poner atributos en JSX, podemos utilizar comillas. 
+// OJO: No debemos confundir con los atributos JS, éstos tienen que ir con llaves y "."
+// ADEMÁS: Si queremos poner un código JSX que ocupe varias líneas, para que nos sea más fácil leerlo y entenderlo, podemos escribirlo entre paréntesis
+element = (
+  <div tabIndex="0">
+    <h1>Hola {getGreeting(user)}</h1>
+    <img src={user.logo} alt="Logo del IES Rafael Alberti" />
+  </div>
+);
+
+element = (
+    <div>
+      <p>Aquí tienes algunos enlaces para saber más de JSX:</p>
+      <ul>
+        <li><a href="https://es.reactjs.org/docs/introducing-jsx.html" target="_blank">[OFICIAL] Introduccion a JSX</a></li>
+        <li><a href="https://carlosazaustre.es/jsx-para-novatos" target="_blank">JSX para novatos</a></li>
+        <li><a href="https://www.w3schools.com/react/react_jsx.asp" target="_blank">[W3Schools] React JSX</a></li>
+        <li><a href="https://dev.to/sapardo/jsx-el-lenguaje-de-react-con-ejemplos-4hc2" target="_blank">Ejemplos JSX</a></li>
+      </ul>
+    </div>
+);
+
+// RENDERIZADO
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);
