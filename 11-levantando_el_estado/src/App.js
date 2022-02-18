1b54ea4aabbf3941a2bca9ad07b901f933591257
Cristina Sanchez García <cristinasanchezgarcia@MacBook-Pro-de-Cristina.local>
2022-02-18 10:43:54 +0100

    levantando el estado

---
+++ 11-levantando_el_estado/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import Calculator from './Calculator.js'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Conversor de temperaturas
+        </p>
+      </header>
+      <div className="App-body">
+        <Calculator />
+      </div>
+    </div>
+  );
+}
+
+export default App;
